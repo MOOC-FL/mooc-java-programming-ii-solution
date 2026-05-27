586b84f5f86b491c7a181ad28d264fb81fa15c30
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2026-05-27 17:24:26 +0300

    try by adding class

--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,25 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
+//import java.util.HashMap;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<books> booksList = new ArrayList<>();
+        while (true) {
+            System.out.println("Input the name of the book, empty stops: ");
+            String word = scanner.nextLine();
+            //int age = Integer.valueOf(scanner.nextLine());
+            if (word.isEmpty()) {
+                break;
+            } else {
+                System.out.println("Input the age recommendation: ");
+                //age = Integer.valueOf(scanner.nextLine());
+                books book = new books(word, age);
+                booksList.add(book);
+            }
+        }
     }
-
 }
---
+++ part10-Part10_14.Literature/src/main/java/books.java
@@ -0,0 +1,14 @@
+public class books {
+    private String name;
+    private int age;
+
+    public books(String name) {
+        this.name = name;
+        this.age = 0;
+    }
+      public books(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
